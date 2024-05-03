dc0a22839952eb3f2bf11677176b0e3ae4f633fc
Devon Browning <browningdevon@gmail.com>
2024-05-02 21:36:54 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# Define tables
+def multiplication_table(n):  # multiplication table *end means it will wait and print at the end
+# TITLE
+    print("\nMultiplication Table:")  # \n means new line?
+
+# COLUMN HEADERS
+    print("    ", end="")  # 'end=""' -- making sure nothing is printed after the spaces in the print statement
+    for i in range(1, n + 1):
+        print(f"{i:4}", end="")  # i:4 specifies 4 digits total
+
+# COLUMN CUTOFF
+    print("\n    =", end="")
+    for i in range(1, n + 1):
+        print("====", end="")
+    print()
+
+# ROW HEADER
+    for i in range(1, n + 1):
+        print(f"{i:2} |", end="")
+
+# INSIDE TABLE
+        for column in range(1, n + 1):
+            print(f"{i * column:4}", end="")
+        print()
+
+def insert_table(n):
+    print("Number   Squared     Cubed")
+    print("======   =======     =====")
+    for i in range(1, n + 1):
+        print(f" {i}\t\t\t{i ** 2}\t\t\t{i ** 3}")
+
+    multiplication_table(n)
+
+print("How well do you know you squares and cubes?")
+
+while True:
+    num = int(input("Please enter an integer: "))
+    insert_table(num)
+    choice = input("Would you like to continue? (y/n)")
+    if choice != "y":
+        break
